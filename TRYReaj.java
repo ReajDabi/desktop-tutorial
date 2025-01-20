5e6fec90145a98108f5fec420061e34d3cda81fe
ReajDabi <r.dabi.147242.tc@umindanao.edu.ph>
2025-01-20 13:25:55 +0800

    Git ni Reaj

---
+++ TRYReaj.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+
+public class TRYReaj {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        System.out.print("Enter your ID number: ");
+        int ans =in.nextInt();
+        
+        System.out.println("Welcome to Git hub student "+ ans);
+    }
+    
+}
